205a00bbbff608b9978c724b5f908291f0711ee5
vincentiadk <50853015+vincentiadk@users.noreply.github.com>
2023-09-11 16:58:13 +0700

    api get subject desc

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -39,7 +39,11 @@ class ContentController extends Controller
             $q_vall .= "q=" . request('q');
         }
         if("null" !==request('sub-class')){
-            $query .= " AND subject_sub_class:" . request('sub-class') . "&sort=field('prob_subject_sub_class')+desc";
+            $subject_sub_class = request('sub-class');
+            if(request('sub-class') == '410'){
+                $subject_sub_class = "[4100 TO 4190]";
+            }
+            $query .= " AND subject_sub_class:" . $subject_sub_class . "&sort=field('prob_subject_sub_class')+desc";
             
             $q_vall .= "&sub-class=" . request('sub-class');
         }
@@ -99,12 +103,19 @@ class ContentController extends Controller
         $response = $this->client->get($this->solr_url. $query);
         $content = $response->getBody()->getContents();
         $doc = json_decode($content, true)["response"]["docs"][0];
+        if(!isset($doc['type']) && (strpos(strtolower($doc['download_original']), "youtube") !== false)){
+            $type = "video";
+        } else if(strpos($doc['download_original'], "journal") !== false) {
+            $type = "article";
+        } else {
+            $type = $doc['type'][0];
+        }
         return response()->json(
             [
                 'id' => $doc['id'],
                 'title' => $doc['title'][0],
                 'description' => isset($doc['description']) ? $doc['description'][0] : '',
-                'type' => isset($doc['type'])? $doc['type'][0] : 'article',
+                'type' => $type,
                 'link' => isset($doc['download_original']) ? $doc['download_original'] : '',
                 'subject_class' => isset($doc['subject_class']) ? $doc['subject_class'][0] : '',
                 'prob_subject_class' => isset($doc['prob_subject_class']) ? $doc['prob_subject_class'][0] : '',
@@ -117,5 +128,33 @@ class ContentController extends Controller
                 'created_at' => isset($doc['created_at']) ? $doc['created_at'] : '',
             ]
         );
+    }
+    function process_csv($file) {
+
+        $file = fopen($file, "r");
+        $data = array();
+    
+        while (!feof($file)) {
+            $data[] = fgetcsv($file, null, ';');
+        }
+    
+        fclose($file);
+        return $data;
+    }
+    public function getSubClassDesc($sub_class)
+    {
+        $data = $this->process_csv('klasifikasi-ddc.csv');
+        $class = substr($sub_class, 0 ,1) . "xx";
+        \Log::info($class);
+        $nama_class = "";
+        foreach($data as $d){
+            if(strtolower($d[0]) == $class){
+                $nama_class = $d[1];
+            }
+            if($d[0] == $sub_class){
+                return 
+                response()->json([ $nama_class , $d[1]]);
+            }
+       }
     }
 }
